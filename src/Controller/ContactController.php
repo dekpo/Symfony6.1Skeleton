05089986e42e870f47b98a025f5ed07e157b063a
Dekpo <dekpo@me.com>
2022-10-19 12:12:43 +0200

    Added form view in templates/contact/index.html.twig

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -6,13 +6,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Form\ContactType;
+use App\Entity\Contact;
+
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
     public function index(): Response
     {
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+
         return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
+            'form' => $form->createView(),
         ]);
     }
 }
