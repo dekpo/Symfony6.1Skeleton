2352ce64ba21f2c93acbe47ec2d4865da4d7a828
Dekpo <dekpo@me.com>
2022-10-19 14:06:30 +0200

    Added a flash message and redirect...

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -9,17 +9,22 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Form\ContactType;
 use App\Entity\Contact;
 use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(Request $request): Response
+    public function index(Request $request,EntityManagerInterface $manager): Response
     {
         $contact = new Contact();
         $form = $this->createForm(ContactType::class, $contact);
         $form->handleRequest($request);
         if($form->isSubmitted() && $form->isValid()){
-            dd($form->getData());
+           $contact = $form->getData();
+           $manager->persist($contact);
+           $manager->flush();
+           $this->addFlash('success','Votre message a  bien été envoyé merci !');
+           return $this->redirectToRoute('app_contact');
         }
 
         return $this->render('contact/index.html.twig', [
