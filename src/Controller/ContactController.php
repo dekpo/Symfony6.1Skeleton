217818f54fde0b0248e932cce7603828bcfe7857
Dekpo <dekpo@me.com>
2022-10-19 12:34:06 +0200

    Handled request in the form...

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -8,14 +8,19 @@ use Symfony\Component\Routing\Annotation\Route;
 
 use App\Form\ContactType;
 use App\Entity\Contact;
+use Symfony\Component\HttpFoundation\Request;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
         $contact = new Contact();
         $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            dd($form->getData());
+        }
 
         return $this->render('contact/index.html.twig', [
             'form' => $form->createView(),
